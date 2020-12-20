2a2cc76fadab9f8cffa8791e05e15c7f85ded418
victoria-berends <tori.berends@gmail.com>
2020-12-20 14:28:07 -0500

    edited readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # final
-simple. idi audio interface with 3 rows of buttons that correspond to the keyboard buttons. each button plays a different sound effect when pressed
-1 row with  play, pause, skip forward and skip back buttons to play 1 of 4 songs
+simple Midi audio interface with 3 rows of buttons that correspond to the keyboard buttons. Each button plays a different sound effect when pressed
+1 row with an audio player with one song loaded, a modal with instructions, and a transparent button the pause the background.
+
+Song is by my friend jessica price eisner, she gave me the song to use.
+sound effects are from http://soundbible.com/about.php they are free to use non comercially.
--- js/main.js
+++ js/main.js
@@ -204,26 +204,26 @@ next.addEventListener(`click`, pressedNext)
 // Start by loading up a new song (but don't play it yet, user must press ▶️)
 playASong(0)*/
 
-// Get the modal
+
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtnModal");
 
-// Get the <span> element that closes the modal
+var btnModal = document.getElementById("myBtnModal");
+
+
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+
+btnModal.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
